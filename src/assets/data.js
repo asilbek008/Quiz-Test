cd0d9d7e190a9401206c450e5ea2ed06bb21bb0e
AsilbekBoyqobilov <asilbekboqyobilov92@gmail.com>
2024-01-18 17:27:20 +0500

    Project Completed

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Which Device is required for the Internet Connection?",
+    Option1: "Modem",
+    Option2: "Router",
+    Option3: "Lan Cable",
+    Option4: "Pen Drive",
+    ans: 1,
+  },
+  {
+    question: "Which Continent has the highest number of countries?",
+    Option1: "Asia",
+    Option2: "Europe",
+    Option3: "North America",
+    Option4: "Africa",
+    ans: 4,
+  },
+  {
+    question: "Junk e-mail is also called?",
+    Option1: "Spam",
+    Option2: "Fake",
+    Option3: "Archived",
+    Option4: "Bin",
+    ans: 1,
+  },
+  {
+    question: "A Computer Cannot BOOT if it does not have the?",
+    Option1: "Application Software",
+    Option2: "Internet",
+    Option3: "Operating System",
+    Option4: "Mouse",
+    ans: 3,
+  },
+  {
+    question: "First Page of Website is termed as?",
+    Option1: "Index Page",
+    Option2: "Homepage",
+    Option3: "Sitemap",
+    Option4: "Pen Drive",
+    ans: 2,
+  },
+];
